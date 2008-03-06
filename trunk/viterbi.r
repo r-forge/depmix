7072bc5bc9a37ca22a0480ddbdba1304876976b5
maarten (Dr ) <m.speekenbrink@ucl.ac.uk>
2008-03-06 17:32:42 +0000

    - viterbi now handles hmmModel - added EM3.R and depmix-test3EM3.R as example of EM with viterbi states for maximisation of trinModel...doesn't work either :(
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/depmix@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/viterbi.r
+++ trunk/viterbi.r
@@ -1,4 +1,43 @@
-viterbi <- function(A,B,prior) {
+viterbi <- function(hmm) {
+    # returns the most likely state sequence
+    nt <- sum(hmm@ntimes)
+    lt <- length(hmm@ntimes)
+		et <- cumsum(hmm@ntimes)
+		bt <- c(1,et[-lt]+1)
+		
+    ns <- hmm@nstates
+    
+    delta <- psi <- matrix(nrow=nt,ncol=ns)
+    state <- vector(length=nt)
+    
+    prior <- exp(logDens(hmm@initModel))
+    
+    A <- hmm@trans
+    B <- apply(hmm@logdens,c(1,3),sum)
+    
+    for(case in 1:lt) {
+    # initialization
+      delta[bt[case],] <- - (log(prior[case,]) + B[bt[case],])
+      psi[bt[case],] <- 0
+      # recursion
+      for(i in ((bt[case]+1):et[case])) {
+          for(j in 1:ns) {
+              delta[i,j] <- min(delta[i-1,] - log(A[i,,j])) - B[i,j]
+              k <- which.min(delta[i-1,] - log(A[i,,j]))
+              if(length(k) == 0) k <- 0
+              psi[i,j] <- k
+          }
+      }
+      #trace maximum likely state
+      state[et[case]] <- which.min(delta[et[case],])
+      for(i in (et[case]-1):bt[case]) {
+          state[i] <- psi[i+1,state[i+1]]
+      }
+    }
+    return(state)
+}
+
+viterbi.fb <- function(A,B,prior) {
     # returns the most likely state sequence
     nt <- nrow(B)
     ns <- ncol(A)
