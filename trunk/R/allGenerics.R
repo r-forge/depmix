2962ea455805d707d35afd4e86913025093a033c
ingmarvisser (Ingmar Visser) <i.visser@uva.nl>
2008-03-07 14:39:32 +0000

    Moved generics into allGenerics.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/depmix@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/R/allGenerics.R
@@ -0,0 +1,39 @@
+
+# 
+# Started by Ingmar Visser, march 2008
+# 
+
+# Guess what: all generics
+
+setGeneric("depmix", function(response=any, transition=any, ...) standardGeneric("depmix"))
+
+setGeneric("nobs", function(object, ...) standardGeneric("nobs"))
+
+setGeneric("ntimes", function(object, ...) standardGeneric("ntimes"))
+
+setGeneric("nstates", function(object, ...) standardGeneric("nstates"))
+
+setGeneric("nresp", function(object, ...) standardGeneric("nresp"))
+
+setGeneric("freepars", function(object, ...) standardGeneric("freepars"))
+
+setGeneric("logLik", function(object, ...) standardGeneric("logLik"))
+
+setGeneric("BIC", function(object, ...) standardGeneric("BIC"))
+
+setGeneric("getdf",function(object) standardGeneric("getdf"))
+
+setGeneric("GLMresponse", function(formula, ...) standardGeneric("GLMresponse"))
+
+setGeneric("transInit", function(formula, ... ) standardGeneric("transInit"))
+
+setGeneric("setpars", function(object,values,which="pars",...) standardGeneric("setpars"))
+
+setGeneric("getpars", function(object,which="pars",...) standardGeneric("getpars"))
+
+setGeneric("fit",function(object,w,...) standardGeneric("fit"))
+
+setGeneric("logDens",function(object,...) standardGeneric("logDens"))
+
+setGeneric("dens",function(object,...) standardGeneric("dens"))
+
