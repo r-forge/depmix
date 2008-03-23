bcfa699ec8d9a8cc5b6ada282bef899e5c95e0b1
ingmarvisser (Ingmar Visser) <i.visser@uva.nl>
2008-03-23 14:11:15 +0000

    Final minor changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/depmix@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/depmixNew-test3.R
+++ trunk/depmixNew-test3.R
@@ -14,21 +14,44 @@ setwd("/Users/ivisser/Documents/projects/depmixProject/depmixNew/rforge/depmix/t
 library(depmixS4)
 
 data(balance)
+# four binary items on the balance scale task
 
 # now fit some latent class models
-trstart=c(1,0,0,1)
+trstart=c(1,0,0,1) # as this is a latent class model, the transition are not optimized
 instart=c(0.5,0.5)
-
-# ntimes is added as an argument
-
 respstart=runif(16)
+# note that ntimes argument is used to make this a mixture model
 mod <- depmix(list(d1~1,d2~1,d3~1,d4~1), data=balance, nstates=2,
 	family=list(multinomial(),multinomial(),multinomial(),multinomial()),
 	respstart=respstart,trstart=trstart,instart=instart,
 	ntimes=rep(1,nrow(balance)))
 
-pars <- getpars(mod)
-fixed <- c(1,0,1,1,1,1,rep(c(1,0),8))
+mod1 <- fit(mod)
+
+# add age as covariate on class membership by using the prior argument
+trstart=c(1,0,0,1) # as this is a latent class model, the transition are not optimized
+instart=c(0.5,0.5,0,0) # we need the initial probs and the coefficients of age 
+respstart=c(rep(c(0.1,0.9),4),rep(c(0.9,0.1),4))
+trstart=c(1,0,0,1)
+mod2 <- depmix(list(d1~1,d2~1,d3~1,d4~1), data=balance, nstates=2,
+	family=list(multinomial(),multinomial(),multinomial(),multinomial()),
+	trstart=trstart, instart=instart, respstart=respstart,
+	ntimes=rep(1,nrow(balance)), prior=~age, initdata=balance)
+
+mod3 <- fit(mod2)
+
+llratio(mod3,mod1)
+
+
+
+# donlp optimzation instead
+
+# mod4 <- fit(mod2,fixed=fixed,method="donlp")
+
+# profiling code
+
+
+
 
 gc()
 Rprof()
@@ -44,37 +67,6 @@ Rprof()
 mod1 <- fit(mod,fixed=fixed)
 Rprof(NULL)
 summaryRprof()
-
-
-mod1
-
-setwd("/Users/ivisser/Documents/projects/depmixProject/depmixNew/rforge/depmix/trunk/")
-
-source("R/depmix.fitted.R")
-source("R/viterbi.R")
-
-
-# 'log Lik.' -1083.036 (df=9)
-
-# 
-# Add age as covariate on class membership
-# 
-
-instart=c(0.5,0.5,0,0)
-respstart=c(rep(c(0.1,0.9),4),rep(c(0.9,0.1),4))
-trstart=c(1,0,0,1)
-mod2 <- depmix(list(d1~1,d2~1,d3~1,d4~1), data=balance, nstates=2,
-	family=list(multinomial(),multinomial(),multinomial(),multinomial()),
-	trstart=trstart, instart=instart, respstart=respstart,
-	ntimes=rep(1,nrow(balance)), prior=~age, initdata=balance)
-
-fixed <- c(1,0,1,0,1,1,1,1,rep(c(1,0),8))
-mod3 <- fit(mod2,fixed=fixed)
-
-llratio(mod3,mod1)
-
-
-mod4 <- fit(mod2,fixed=fixed,method="donlp")
 
 
 
