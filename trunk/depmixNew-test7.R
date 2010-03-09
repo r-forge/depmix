f04cc34e51aca7a42f2d1b44700765ace063fd5e
ingmarvisser (Ingmar Visser) <i.visser@uva.nl>
2010-03-09 09:25:37 +0000

    tests for solnp/donlp/em control objects
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/depmix@396 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/depmixNew-test7.R
@@ -0,0 +1,33 @@
+
+# 
+# Started by Ingmar Visser 17-02-2010
+# 
+# Usage: go to trunk directory and source("depmixNew-test6.R")
+# 
+
+setwd("~/Documents/projects/depmixProject/codesvn/depmix/trunk/")
+
+require(depmixS4)
+
+data(speed)
+
+
+rsolnp control list:
+
+rho
+Penalty parameter (default 1)
+outer.iter
+Maximum number of major (outer) iterations (default 400)
+inner.iter
+Maximum number of minor (inner) iterations (default 800)
+delta
+Relative step size in forward difference evaluation (default 1.0e-8)
+tol
+Tolerance on feasibility and optimality (default 1e-6)
+trace
+The value of the objective function and the parameters is printed at every major iteration (default 0)
+
+
+
+
+
